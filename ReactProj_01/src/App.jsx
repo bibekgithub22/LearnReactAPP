b1ff440b83274516ac9f4912537ba1f7c82ce8a9
Bibek Biswas <jbiswas94236@gmail.com>
2024-09-17 21:22:24 +0530

    update Navbar.jsx

--- ReactProj_01/src/App.jsx
+++ ReactProj_01/src/App.jsx
@@ -1,8 +1,9 @@
+import { NavbarComp } from "./NavbarComp";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <NavbarComp/>
     </>
   );
 }
---
+++ ReactProj_01/src/NavbarComp.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./index.css";
+export const NavbarComp = () => {
+  return (
+    <div id="nav">
+      <div className="left">
+        <h2>LOGO</h2>
+      </div>
+      <div className="mid">
+        <ul>
+          <li>Service</li>
+          <li>Product</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div className="right">
+        <input type="search" name="" id="search" placeholder="Search..." />
+      </div>
+    </div> 
+  );
+ 
+};
