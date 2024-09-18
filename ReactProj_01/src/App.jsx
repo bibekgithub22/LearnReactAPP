5ff084f6356c1a8f17a7cde83fd915aa743ed2a7
Bibek Biswas <jbiswas94236@gmail.com>
2024-09-18 23:30:52 +0530

    made the members card with md:flex

--- ReactProj_01/src/App.jsx
+++ ReactProj_01/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <NavbarComp />
       <div className="w-full min-h-[calc(100vh-3.5rem)] relative overflow-hidden">
         <video
-          src="https://videocdn.cdnpk.net/videos/a028eda0-1603-4c80-b345-a29a5829884d/horizontal/previews/clear/large.mp4?token=exp=1726674160~hmac=bca1c53d4c02ebd30b93a7ff88ac6dd3c899d31053f094f46d097a07e1587490"
+          src="https://videocdn.cdnpk.net/videos/a028eda0-1603-4c80-b345-a29a5829884d/horizontal/previews/clear/large.mp4?token=exp=1726677187~hmac=bf7ac745daf76ab658299a79da1011436a9a0252357cd65e953b70c28db807ee"
           autoPlay
           loop
           muted
@@ -18,19 +18,21 @@ function App() {
           Your browser does not support the video tag.
         </video>
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40">
-          <h1
-            
-            className="text-white text-3xl font-bold px-4 text-center"
-          >
-          Smart India Hackathon
+          <h1 className="text-white text-3xl font-bold px-4 text-center">
+            Smart India Hackathon
           </h1>
         </div>
       </div>
       {/* 2nd page */}
       <div className="h-[100vh] w-auto text-white ">
         <div>
-        <h1 className="text-center font-bold p-6 pt-10 text-[1.8rem] mt-10  leading-loose">INTRODUCING<br /><span className="text-sky-600">TEAM HACKNOVICES</span></h1>
-       <Members />
+          <h1 className="text-center font-bold p-6 pt-10 text-[1.8rem] mt-10  leading-loose">
+            INTRODUCING
+            <br />
+            <span className="text-sky-600">TEAM HACKNOVICES</span>
+          </h1>
+
+          <Members />
         </div>
       </div>
     </>
--- ReactProj_01/src/Members.jsx
+++ ReactProj_01/src/Members.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
 export default function Members() {
   return (
-    <div className='h-fit w-[auto] bg-slate-800 md:flex md:flex-wrap' >
+    <>
+      <div className="h-fit w-[100%] flex flex-col items-center  md:flex-row md:justify-evenly md:flex-wrap">
+        <div
+          className="h-[12rem] w-[12rem] my-10 mx-10 rounded-lg bg-[url('./public/img/2.jpg')] bg-cover bg-center"
+          role="img"
+          aria-label="Decorative background image"
+        ></div>
 
-    </div>
-  )
+        <div
+          className="h-[12rem] w-[12rem] my-10 mx-10 rounded-lg bg-[url('./public/img/1.jpg')] bg-cover bg-center"
+          role="img"
+          aria-label="Decorative background image"
+        ></div>
+
+        <div
+          className="h-[12rem] w-[12rem] my-10 mx-10 rounded-lg bg-[url('./public/img/3.jpg')] bg-cover bg-center"
+          role="img"
+          aria-label="Decorative background image"
+        ></div>
+
+        <div
+          className="h-[12rem] w-[12rem] my-10 mx-10 rounded-lg bg-[url('./public/img/4.jpg')] bg-cover bg-center"
+          role="img"
+          aria-label="Decorative background image"
+        ></div>
+
+        <div
+          className="h-[12rem] w-[12rem] my-10 mx-10 rounded-lg bg-[url('./public/img/5.jpg')] bg-cover bg-center"
+          role="img"
+          aria-label="Decorative background image"
+        ></div>
+
+        <div
+          className="h-[12rem] w-[12rem] my-10 mx-10 rounded-lg bg-[url('./public/img/6.jpg')] bg-cover bg-center"
+          role="img"
+          aria-label="Decorative background image"
+        ></div>
+      </div>
+    </>
+  );
 }
